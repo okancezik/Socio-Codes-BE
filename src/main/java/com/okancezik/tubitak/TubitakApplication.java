a39a76b39b3ebf60bca149893a5d0ba6baa94a69
Okan Cezik <okacezik@gmail.com>
2024-02-10 12:22:59 +0300

    init

---
+++ src/main/java/com/okancezik/tubitak/TubitakApplication.java
@@ -0,0 +1,13 @@
+package com.okancezik.tubitak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TubitakApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TubitakApplication.class, args);
+	}
+
+}
