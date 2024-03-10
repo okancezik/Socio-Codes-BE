524c4aad8af07f49e2db65692074664556d0b2e0
Okan Cezik <okacezik@gmail.com>
2024-03-10 16:33:14 +0300

    added jwt service and jwt auth filter

--- src/main/java/com/okancezik/tubitak/dataAccess/UserRepository.java
+++ src/main/java/com/okancezik/tubitak/dataAccess/UserRepository.java
@@ -3,5 +3,10 @@ package com.okancezik.tubitak.dataAccess;
 import com.okancezik.tubitak.entity.concretes.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User,Integer> {
+
+    Optional<User> findByEmail(String email);
+
 }
--- src/main/java/com/okancezik/tubitak/entity/concretes/User.java
+++ src/main/java/com/okancezik/tubitak/entity/concretes/User.java
@@ -72,7 +72,7 @@ public class User implements UserDetails {
     private Role role;
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return List.of(new SimpleGrantedAuthority(role.name()))
+        return List.of(new SimpleGrantedAuthority(role.name()));
     }
 
     @Override
